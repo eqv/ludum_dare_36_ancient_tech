26b7b6182de3401f0a1a611b4b264c0e8d7d550d
coco <coco@hexgolems.com>
2016-08-27 13:07:32 +0200

    added some programmers art

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import GameState from 'states/GameState';
+import TrackState from 'states/TrackState';
+
+function preload(){
+  alert(1);
+}
 
 class Game extends Phaser.Game {
 
+
 	constructor() {
-		super(500, 500, Phaser.AUTO, 'content', null);
-		this.state.add('GameState', GameState, false);
-		this.state.start('GameState');
+		super(500, 500, Phaser.AUTO, 'content', null, {preload: preload});
+		this.state.add('TrackState', TrackState, false);
+		this.state.start('TrackState');
 	}
+
 
 }
 
