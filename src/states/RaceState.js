2fe70785a61576746e92d0952240bff8c5e2455b
Vladislav Supalov <vladislav@supalov.com>
2016-08-27 14:45:54 +0200

    Indent.

--- src/states/RaceState.js
+++ src/states/RaceState.js
@@ -2,34 +2,34 @@ import TrackInfo from 'objects/TrackInfo';
 
 class RaceState extends Phaser.State {
 
-	create() {
-    this.game.stage.backgroundColor = '#124184';
+    create() {
+        this.game.stage.backgroundColor = '#124184';
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-    let trackinfo = new TrackInfo(this.game, "track1")
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.game.world.setBounds(0, 0, 2000, 2000);
+        let trackinfo = new TrackInfo(this.game, "track1")
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.game.world.setBounds(0, 0, 2000, 2000);
 	}
 
-  update() {
-      if (this.cursors.up.isDown)
-      {
-          this.game.camera.y -= 4;
-      }
-      else if (this.cursors.down.isDown)
-      {
-          this.game.camera.y += 4;
-      }
+    update() {
+        if (this.cursors.up.isDown)
+        {
+            this.game.camera.y -= 4;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.game.camera.y += 4;
+        }
 
-      if (this.cursors.left.isDown)
-      {
-          this.game.camera.x -= 4;
-      }
-      else if (this.cursors.right.isDown)
-      {
-          this.game.camera.x += 4;
-      }
+        if (this.cursors.left.isDown)
+        {
+            this.game.camera.x -= 4;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.game.camera.x += 4;
+        }
 
-  }
+    }
 
 }
 
