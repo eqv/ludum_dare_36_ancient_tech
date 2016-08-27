5a49a5ac139ba609676c6a4624a36ef4111df49b
Vladislav Supalov <vladislav@supalov.com>
2016-08-27 18:41:10 +0200

    Works.

--- src/objects/VectorAim.js
+++ src/objects/VectorAim.js
@@ -4,14 +4,18 @@ class VectorAim extends Phaser.Graphics {
 
         this.previous_positions = [];
         this.velocity = new Phaser.Point(0, 0);
+        this.velocity_change = new Phaser.Point(0, 0);
 
+        // is being set in update and used in render
         this.world_mouse_coords = new Phaser.Point(0, 0);
 
+        this.max_acceleration = 25;
+
+        // whether the left mouse button has been pressed during the last update
         this.formerMouseDown = false;
 
-        this.fixedToCamera = false;
+        //this.fixedToCamera = false;
         this.render();
-
 		this.game.world.add(this);
 	}
 
@@ -19,6 +23,14 @@ class VectorAim extends Phaser.Graphics {
         var pointer = this.game.input.mousePointer; //activePointer;
         this.world_mouse_coords.x = pointer.worldX;
         this.world_mouse_coords.y = pointer.worldY;
+
+        this.velocity_change.x = this.world_mouse_coords.x-(this.x+this.velocity.x);
+        this.velocity_change.y = this.world_mouse_coords.y-(this.y+this.velocity.y);
+        var mag = this.velocity_change.getMagnitude();
+        if (mag > this.max_acceleration) {
+            this.velocity_change.setMagnitude(this.max_acceleration);
+        }
+            
 
         //TODO: this method sucks. It's not always registered :(
         if (!pointer.leftButton.isDown && this.formerMouseDown) {
@@ -28,13 +40,8 @@ class VectorAim extends Phaser.Graphics {
 
             this.previous_positions.push(new Phaser.Point(this.x, this.y));
 
-            var velocity_change = new Phaser.Point(
-                this.world_mouse_coords.x-(this.x+this.velocity.x),
-                this.world_mouse_coords.y-(this.y+this.velocity.y)
-            );
-            
-            this.velocity.x += velocity_change.x;
-            this.velocity.y += velocity_change.y;
+            this.velocity.x += this.velocity_change.x;
+            this.velocity.y += this.velocity_change.y;
 
             this.x += this.velocity.x;
             this.y += this.velocity.y;
@@ -66,7 +73,7 @@ class VectorAim extends Phaser.Graphics {
                 this.lineTo(point_x, point_y);
             }
 
-            // paint points
+            // paint points on the path
             for (var i = this.previous_positions.length; i > 0 ; i--) {
                 var point_x = this.previous_positions[i-1].x-this.x;
                 var point_y = this.previous_positions[i-1].y-this.y;
@@ -80,17 +87,18 @@ class VectorAim extends Phaser.Graphics {
         // circle around future position
         this.lineStyle(1, 0xffffff);
         this.beginFill(0xff0000, 0.3);
-        this.drawCircle(this.velocity.x, this.velocity.y, 50);
+        this.drawCircle(this.velocity.x, this.velocity.y, this.max_acceleration*2);
         this.endFill();
 
-        this.lineStyle(1, 0xffff00);
-        //interactive mouse thingy
-        this.moveTo(this.velocity.x,this.velocity.y);
-        this.lineTo(this.world_mouse_coords.x-this.x, this.world_mouse_coords.y-this.y);
-
+        // line to future position
         this.lineStyle(3, 0x00ff00);
         this.moveTo(0,0);
         this.lineTo(this.velocity.x, this.velocity.y);
+
+        // interactive mouse thingy, cap the line at the circle radius
+        this.lineStyle(1, 0xffff00);
+        this.moveTo(this.velocity.x, this.velocity.y);
+        this.lineTo(this.velocity.x+this.velocity_change.x, this.velocity.y+this.velocity_change.y);
     }
 }
 
