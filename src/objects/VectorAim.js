9620953ce34105e8b93b7dfa8d2909877308037b
Vladislav Supalov <vladislav@supalov.com>
2016-08-27 17:09:21 +0200

    Less long line.

--- src/objects/VectorAim.js
+++ src/objects/VectorAim.js
@@ -3,7 +3,7 @@ class VectorAim extends Phaser.Graphics {
 		super(game, x, y);
 
         //this._points = [];
-        this.p0 = new Phaser.Point(0, 0);
+        this.world_mouse_coords = new Phaser.Point(0, 0);
         //this._points.push(p0);
 
         this.fixedToCamera = false;
@@ -14,16 +14,16 @@ class VectorAim extends Phaser.Graphics {
 
     update() {
         var pointer = this.game.input.mousePointer; //activePointer;
-        this.p0.x = pointer.worldX;
-        this.p0.y = pointer.worldY;
-        this.render()
+        this.world_mouse_coords.x = pointer.worldX;
+        this.world_mouse_coords.y = pointer.worldY;
+        this.render();
     }
 
     render() {
         this.clear();
 
-        var object_x = this.x;
-        var object_y = this.y;
+        var world_object_coords = new Phaser.Point(this.x, this.y);
+        var world_line_end = new Phaser.Point(0,0);
 
         this.lineStyle(1, 0xffff00);
 
@@ -33,14 +33,14 @@ class VectorAim extends Phaser.Graphics {
 
         this.lineStyle(1, 0xffff00);
         this.moveTo(0,0);
-        this.lineTo(this.p0.x-object_x, this.p0.y-object_y);
+        this.lineTo(this.world_mouse_coords.x-object_x, this.world_mouse_coords.y-object_y);
 
         this.beginFill(0xffff00);
         this.drawRect(0-2, 0-2, 4, 4);
         this.endFill();
 
         this.beginFill(0xfff000);
-        this.drawRect(this.p0.x-object_x-2, this.p0.y-object_y-2, 4, 4);
+        this.drawRect(this.world_mouse_coords.x-object_x-2, this.world_mouse_coords.y-object_y-2, 4, 4);
         this.endFill();
     }
 }
