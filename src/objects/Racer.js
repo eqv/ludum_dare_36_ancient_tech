afcdefd39449415e7b30fd12166c6d271e40f4af
Eugen Beck <eugen.beck@rwth-aachen.de>
2016-08-28 17:59:58 +0200

    Fix whitespace

--- src/objects/Racer.js
+++ src/objects/Racer.js
@@ -1,5 +1,5 @@
 class RacerPhysics {
-  constructor(x,y, racer, trackinfo){
+  constructor(x, y, racer, trackinfo) {
         this.x = x;
         this.y = y;
         this.racer = racer
@@ -76,7 +76,7 @@ class Racer extends Phaser.Graphics {
     constructor(game, x, y, trackinfo) {
         super(game, x, y);
 
-        this.physics = new RacerPhysics(x,y, this, trackinfo)
+        this.physics = new RacerPhysics(x, y, this, trackinfo)
         this.previous_positions = [];
         this.history_length = 7;
 
@@ -84,7 +84,7 @@ class Racer extends Phaser.Graphics {
         this.game.world.add(this);
     }
 
-    fork_physics(){
+    fork_physics() {
       let res = new Racer(this.game, this.x, this.y, this.trackinfo)
       res.velocity = this.velocity.clone();
       res.acceleration = this.acceleration.clone();
@@ -93,11 +93,11 @@ class Racer extends Phaser.Graphics {
       res.max_acceleration = this.max_acceleration.clone();
     }
 
-    set_acceleration(accel){
+    set_acceleration(accel) {
       this.physics.set_acceleration(accel)
     }
 
-    move(){
+    move() {
       let prev_pos = new Phaser.Point(this.physics.x, this.physics.y);
       this.previous_positions.push(prev_pos);
       this.physics.move()
@@ -127,19 +127,19 @@ class Racer extends Phaser.Graphics {
 
             // paint line path
             for (var i = this.previous_positions.length; i > last_idx; i--) {
-                var point_x = this.previous_positions[i-1].x-this.x;
-                var point_y = this.previous_positions[i-1].y-this.y;
+                var point_x = this.previous_positions[i-1].x - this.x;
+                var point_y = this.previous_positions[i-1].y - this.y;
 
                 this.lineTo(point_x, point_y);
             }
 
             // paint points on the path
             for (var i = this.previous_positions.length; i > last_idx; i--) {
-                var point_x = this.previous_positions[i-1].x-this.x;
-                var point_y = this.previous_positions[i-1].y-this.y;
+                var point_x = this.previous_positions[i-1].x - this.x;
+                var point_y = this.previous_positions[i-1].y - this.y;
 
                 this.beginFill(0x00ff00);
-                this.drawRect(point_x-2, point_y-2, 4, 4);
+                this.drawRect(point_x - 2, point_y - 2, 4, 4);
                 this.endFill();
             }
         }
